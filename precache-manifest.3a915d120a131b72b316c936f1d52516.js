9a18264c86709a9d62b390c7bd6d3d93d0bfad03
Zubair <71716740+Zubair-gulbarge@users.noreply.github.com>
2024-08-04 08:34:04 +0530

    Updates

---
+++ precache-manifest.3a915d120a131b72b316c936f1d52516.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b99e20a844f17b7abba8979484282761",
+    "url": "/gulbarge-tech/index.html"
+  },
+  {
+    "revision": "564fd35cea40579784d7",
+    "url": "/gulbarge-tech/static/css/2.b05a74ed.chunk.css"
+  },
+  {
+    "revision": "d166817b806177d19aa8",
+    "url": "/gulbarge-tech/static/css/main.fdb74a15.chunk.css"
+  },
+  {
+    "revision": "564fd35cea40579784d7",
+    "url": "/gulbarge-tech/static/js/2.4aa27237.chunk.js"
+  },
+  {
+    "revision": "6cb2abd69bcf5c1bc8ea13bc6a30ce0e",
+    "url": "/gulbarge-tech/static/js/2.4aa27237.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d31f3d03dff782ca762b",
+    "url": "/gulbarge-tech/static/js/3.e6cda43d.chunk.js"
+  },
+  {
+    "revision": "d166817b806177d19aa8",
+    "url": "/gulbarge-tech/static/js/main.eaddf65e.chunk.js"
+  },
+  {
+    "revision": "1ff611b4ff21fd765b8b",
+    "url": "/gulbarge-tech/static/js/runtime-main.07b6a857.js"
+  },
+  {
+    "revision": "4ea3794d631bedab7999678f46b0a162",
+    "url": "/gulbarge-tech/static/media/CentraNo2-Bold.4ea3794d.ttf"
+  },
+  {
+    "revision": "386f05946a76935fcabf319affff517a",
+    "url": "/gulbarge-tech/static/media/CentraNo2-Book.386f0594.ttf"
+  },
+  {
+    "revision": "63911e8535278ced52b8a37e819f6f5d",
+    "url": "/gulbarge-tech/static/media/CentraNo2-Medium.63911e85.ttf"
+  },
+  {
+    "revision": "113045a25ccd88a045e308dbc4facec4",
+    "url": "/gulbarge-tech/static/media/Project1.113045a2.png"
+  },
+  {
+    "revision": "0cc218e51163cb8507ebf164ca2fca55",
+    "url": "/gulbarge-tech/static/media/Project2.0cc218e5.png"
+  },
+  {
+    "revision": "5f3ca49706055342e92708c7dd31849f",
+    "url": "/gulbarge-tech/static/media/banner-bg.5f3ca497.png"
+  },
+  {
+    "revision": "d64094774a77a02c2c22356681131cb8",
+    "url": "/gulbarge-tech/static/media/color-sharp.d6409477.png"
+  },
+  {
+    "revision": "42c4dbf649597b4d4ceaf4c9792de60a",
+    "url": "/gulbarge-tech/static/media/color-sharp2.42c4dbf6.png"
+  },
+  {
+    "revision": "a8822e204211be92b2412ccbe284e785",
+    "url": "/gulbarge-tech/static/media/contact-img.a8822e20.svg"
+  },
+  {
+    "revision": "e9228c17c13a87653f1b8ed01a3ff306",
+    "url": "/gulbarge-tech/static/media/footer-bg.e9228c17.png"
+  },
+  {
+    "revision": "bfe6d1a6d579d73c0c625ec0afe2855b",
+    "url": "/gulbarge-tech/static/media/header-img.bfe6d1a6.svg"
+  },
+  {
+    "revision": "fd2cc328bad752557b2ea1ef4a68d006",
+    "url": "/gulbarge-tech/static/media/icons8-hashnode.fd2cc328.svg"
+  },
+  {
+    "revision": "04385a2419698a51b55418760b57220b",
+    "url": "/gulbarge-tech/static/media/logo.04385a24.svg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/gulbarge-tech/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "dea05c56b45857a24dcba5a23202ec70",
+    "url": "/gulbarge-tech/static/media/meter1.dea05c56.svg"
+  },
+  {
+    "revision": "7fb1b05f49ba5c3b4570e1dec3728213",
+    "url": "/gulbarge-tech/static/media/meter2.7fb1b05f.svg"
+  },
+  {
+    "revision": "19e4204daee7b06ccd0aa7807bc37653",
+    "url": "/gulbarge-tech/static/media/meter3.19e4204d.svg"
+  },
+  {
+    "revision": "dff2b6246274897c980cd8a354f7f979",
+    "url": "/gulbarge-tech/static/media/nav-icon1.dff2b624.svg"
+  },
+  {
+    "revision": "5943002587e85861de38644a5859ee41",
+    "url": "/gulbarge-tech/static/media/nav-icon2.59430025.svg"
+  },
+  {
+    "revision": "70af9240bb95dd2d65f6bf9f09591507",
+    "url": "/gulbarge-tech/static/media/project-img1.70af9240.png"
+  },
+  {
+    "revision": "98b3bc237950d6cb1a1e49f75372ae36",
+    "url": "/gulbarge-tech/static/media/project-img2.98b3bc23.png"
+  },
+  {
+    "revision": "08e5b04289e11fe1c0776cef35c4bcf2",
+    "url": "/gulbarge-tech/static/media/project-img3.08e5b042.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/gulbarge-tech/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/gulbarge-tech/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/gulbarge-tech/static/media/revicons.2feb69cc.eot"
+  }
+]);
